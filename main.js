2fe4a16958136705e599ab8efb4b9a058a16565e
Marcos Felippe <mfelippe.bsb@hotmail.com>
2021-01-08 13:16:32 -0300

    Criando Menu para aplicação

--- main.js
+++ main.js
@@ -84,6 +84,7 @@ async function openPainel() {
 
   await painel.loadFile('./src/pages/editor/painel.html');
 
+
 }
 
 // Janela Princioal
@@ -100,6 +101,7 @@ async function createWindow() {  //asyn pq tem funções assincronas
     }
 
   });
+  Menu.setApplicationMenu(menu1);
   await mainWindow.loadFile('./src/pages/editor/login.html');
 }
 
@@ -107,41 +109,33 @@ async function createWindow() {  //asyn pq tem funções assincronas
 var file = {};
 
 // funções do menu
-function createNewFile() {
-  // função para criar um novo arquivo no programa
-  file = {
-    name: 'novo-arquivo.txt',
-    content: '',
-    saved: false,
-    path: app.getPath('documents') + '/novo-arquivo.txt'
-  };
+function abrirPainel() {
 
-  mainWindow.webContents.send('set-file', file);
 }
 
-/* template Menu
-const templateMenu = [{
-      label: 'Arquivo',
-      submenu: [{
-        label: 'Novo',
-        click() {
-          //configurando uma função quando for criado no clique no menu
-          createNewFile();
-        }
-      }, {label: 'Abrir'}, {label: 'Salvar'}]
-    }, {
-      label: 'Fechar',
-      role: process.platform === 'darwin' ? 'close' : 'quit'
-    }] // faz a verificação se é mac or windows;
+// template Menu
+const templateMenuLogin = [{
+  /*label: 'DashBoard',
+  submenu: [{
+    label: 'Novo',
+    click() {
+      //configurando uma função quando for criado no clique no menu
+      createNewFile();
+    }
+  }]
+}, {
+  */label: 'Fechar',
+  role: process.platform === 'darwin' ? 'close' : 'quit'
+}] // faz a verificação se é mac or windows;
 
 // Menu
-//const menu = Menu.buildFromTemplate(templateMenu);
-Menu.setApplicationMenu(menu);
-*/
+const menu1 = Menu.buildFromTemplate(templateMenuLogin);
+
+
 //ON READY
 app.whenReady().then(createWindow);
 
-//activate
+//activate faz abertura de janela no MascOS
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().lenght === 0) {
     createWindow();
